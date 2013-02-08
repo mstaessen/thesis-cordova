df91d5c7fbe9dc2a2d241480852aa7b056692347
Michiel Staessen <staessenmichiel@gmail.com>
2013-02-08 17:01:20 +0100

    Cordangular skeleton

---
+++ assets/js/angular-cordova.js
@@ -0,0 +1,56 @@
+angular
+    .module('ngCordova', [])
+    .factory('Navigator', function() {
+
+    })
+    .factory('Accelerometer', function () {
+
+    })
+    .factory('Camera', function () {
+
+    })
+    .factory('Capture', function () {
+
+    })
+    .factory('Compass', function () {
+
+    })
+    .factory('Connection', function () {
+
+    })
+    .factory('Contacts', function () {
+
+    })
+    .factory('Device', function () {
+
+    })
+    .factory('Events', function () {
+
+    })
+    .factory('File', function () {
+
+    })
+    .factory('Geolocation', function () {
+
+    })
+    .factory('Globalization', function () {
+
+    })
+    .factory('InAppBrowser', function () {
+
+    })
+    .factory('Media', function () {
+
+    })
+    .factory('Notification', function () {
+
+    })
+    .factory('SplashScreen', function () {
+
+    })
+    .factory('Storage', function () {
+
+    })
+    .run(function (Navigator) {
+
+    });
